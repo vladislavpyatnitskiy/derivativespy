75ea9af18bb32b869ef05658bcb8ecdda0554c8f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-08 15:24:34 +0500

    Create Generalised one step binomial tree.R   Latest commit vladislavpyatnitskiy vladislavpyatnitskiy Update Generalised one step binomial tree.py
    Added script to calculate option price in python

---
+++ Generalised one step binomial tree.R   Latest commit vladislavpyatnitskiy vladislavpyatnitskiy Update Generalised one step binomial tree.py
@@ -0,0 +1,10 @@
+# Function to calculate option value via generalisation
+def gen_one_step_bin_tree(y, s, d, r):
+  
+  bins = (np.exp(r * y) - (s - d) / s) / (2 * d / s) # Calculate probability
+  
+  option = round(np.exp(-r*y) * (bins + 0*(1 - bins)),3)
+  
+  print(f"Option Value today is {option}")
+
+gen_one_step_bin_tree(y = .25, s = 20, d = 2, r = .12) # Test
