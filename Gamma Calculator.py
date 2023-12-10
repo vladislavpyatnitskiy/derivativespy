a7fb1083e50b2be4fcfffc0644191232f374a847
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-10 12:58:35 +0500

    Create Gamma Calculator.py
    Added script to calculate Gamma of the option via python

---
+++ Gamma Calculator.py
@@ -0,0 +1,11 @@
+def gamma_calculator(S, K, v, y, r):
+    d1 = (np.log(S / K) + (r + v ** 2 / 2) * y) / (v * y ** 0.5)  # Delta 1
+    d2 = d1 - v * y ** 0.5  # Delta 2
+
+    d_d1 = np.exp(-d1 ** 2 / 2) / (2 * np.pi) ** 0.5  # Derived Delta 1
+
+    gamma_parameter = d_d1 / S / v / y ** 0.5  # Gamma value
+
+    return {"Call & Put": gamma_parameter}  # Display value
+
+gamma_calculator(49, 50, 0.2, 0.3846, 0.05) # Example usage:
